a1d912211606aab12f37190b58d98d19d5b99a2a
pswd123456 <92856801+pswd123456@users.noreply.github.com>
2022-07-06 20:57:39 +0800

    1

--- BookShop/User/SearchResult.aspx.cs
+++ BookShop/User/SearchResult.aspx.cs
@@ -23,28 +23,24 @@ public partial class User_SearchResult : System.Web.UI.Page
         string P_Str_ClassName = mcObj.GetClass(P_Int_ClassID);
         return P_Str_ClassName;
     }
-    /// <summary>
-    /// 显示指定类别的图书信息
-    /// </summary>
+
     public void dlSearchBind()
     {
         ucObj.DCSearchBind(Request.QueryString["SearchName"].ToString(), "SearchName", DLsearch);
     }
-    //绑定市场价格
+
     public string GetVarMKP(string P_Str_MarketPrice)
     {
         return ucObj.VarStr(P_Str_MarketPrice, 2);
     }
-    //绑定会员价格
+
     public string GetVarMBP(string P_Str_MemberPrice)
     {
         return ucObj.VarStr(P_Str_MemberPrice, 2);
     }
 
-    //当购买图书时，获取图书信息
     public SaveSubBookClass GetSubBookInformation(DataListCommandEventArgs e, DataList DLName)
     {
-        //获取购物车中的信息
         SaveSubBookClass Book = new SaveSubBookClass();
         Book.BookID = int.Parse(DLName.DataKeys[e.Item.ItemIndex].ToString());
         string BookStyle = e.CommandArgument.ToString();
@@ -62,19 +58,19 @@ public partial class User_SearchResult : System.Web.UI.Page
             Book = GetSubBookInformation(e, DLName);
             if (Book == null)
             {
-                //显示错误信息
-                Response.Write("<script>alert('没有可用的数据');</script>");
+    
+                Response.Write("<script>alert('No data');</script>");
                 return;
             }
             else
             {
                 ucObj.AddShopCart(Book.BookID, Book.MemberPrice, Convert.ToInt32(Session["UID"].ToString()));
-                Response.Write("<script>alert('恭喜您，添加成功！')</script>");
+                Response.Write("<script>alert('Add success！')</script>");
             }
         }
         else
         {
-            Response.Write("<script>alert('请先登录，谢谢合作！');</script>");
+            Response.Write("<script>alert('Please login first！');</script>");
 
         }
 
